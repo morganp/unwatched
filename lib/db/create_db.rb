b2293ab12549ad4e301df46519c589d44b266e7d
Morgan Prior <morgan@amaras-tech.co.uk>
2010-09-11 21:55:29 +0100

    Added defaults to the file extensions table

--- lib/db/create_db.rb
+++ lib/db/create_db.rb
@@ -5,17 +5,21 @@ require 'sequel'
 DB = Sequel.sqlite('./unwatched.db')
 
 ##########################################################
-  DB.create_table :nodes do
-    primary_key :id
-    foreign_key :parent_id, :nodes
-    varchar :name
-  end
+   DB.create_table :nodes do
+      primary_key :id
+      foreign_key :parent_id, :nodes
+      varchar :name
+   end
 
-  DB.create_table :extensions do
+   DB.create_table :extensions do
       primary_key    :id
       varchar        :ext
-  end
+   end
 
+   DB[:extensions].insert(:ext => 'avi')
+   DB[:extensions].insert(:ext => 'mkv')
+   DB[:extensions].insert(:ext => 'mov')
+   DB[:extensions].insert(:ext => 'm4v')
 
 
 ##########################################################
