b858734e49dd942806e8f75dcdd7f00c0a0f2f74
Morgan Prior <morgan@amaras-tech.co.uk>
2010-10-19 20:23:47 +0100

    Reworked order of functions for watched/unwatched functionality

--- lib/app.rb
+++ lib/app.rb
@@ -80,10 +80,23 @@ module UnWatched
       ### Main Route 
       ####################################
       get '/*' do
+         @url                    = params['splat'].to_s
+
+          #Determine mode from url 
+          @mode, @merged, @sort   = discover_mode_from_url( @url )
+         
          @path      = get_path( params['path'] )
-           
+          
+         mark    =  params['mark']
+         mark    ||= false
+         puts mark
          #Check if Media file, and launch if required.   
-         media_test = open_media( @path )
+         if not mark
+            media_test = open_media( @path )
+         else
+            #Mark files appropriatelly
+            media_test = mark_media( mark, @path, @mode)
+         end
             
          if media_test[:media]
             ## Redirect, do not include the file to be launched.
@@ -91,10 +104,10 @@ module UnWatched
             redirect "/" + params["splat"].to_s + "?path=" +  media_test[:path]
 
          else
-            @url                    = params['splat'].to_s
+            #@url                    = params['splat'].to_s
      
             #Determine mode from url 
-            @mode, @merged, @sort   = discover_mode_from_url( @url ) 
+            #@mode, @merged, @sort   = discover_mode_from_url( @url ) 
                    
             ## Create base_urls
             @url_normal, @url_safe  = create_base_url( @url, @mode ) 
@@ -106,6 +119,7 @@ module UnWatched
             @url_merged             = create_merged_url( @url, @merged )
 
             @files                  = get_files( @path.dup, @mode, @sort )
+
             erb :all
          end
       end
