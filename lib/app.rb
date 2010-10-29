5b9d39cd6c7b5e7fc683fa3b2dc9e403b21abdc7
Morgan Prior <morgan@amaras-tech.co.uk>
2010-10-29 01:12:04 +0100

    Added sinatra session helper

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,8 @@
 
 require 'rubygems'
 require 'sinatra/base'
+require 'sinatra/session'
+
 
 require 'active_record'
 require 'model/node'
@@ -10,118 +12,140 @@ require 'helpers/browser_helper'
 #For the file name escaping unescaping
 require 'cgi'
 
+require 'pp'
+
 module UnWatched
 
-   ## Modes
-   # These are the symbols used to denote the modes
-   #:normal
-   #:safe
-   #:merged          
-   #:sort_mod_asc    
-   #:sort_mod_desc   
-   #:sort_alpha_asc  
-   #:sort_alpha_desc 
+  ## Modes
+  # These are the symbols used to denote the modes
+  #:normal
+  #:safe
+  #:merged          
+  #:sort_mod_asc    
+  #:sort_mod_desc   
+  #:sort_alpha_asc  
+  #:sort_alpha_desc 
 
-   DATE_FORMAT = "%I:%M%p %b %d %Y"
+  DATE_FORMAT = "%I:%M%p %b %d %Y"
 
-   class App < Sinatra::Base
-         
-      
-      #Mixin extra functions
-      include UnWatched::BrowserHelper
-      
-      set :public, "public"
+  class App < Sinatra::Base
+    register Sinatra::Session
 
-      configure :production  do
-         ActiveRecord::Base.establish_connection(
-            :adapter    => 'sqlite3',
-            :database   => File.dirname( __FILE__) + '/db/unwatched.db'
-         )
+
+
+    #Mixin extra functions
+    include UnWatched::BrowserHelper
+
+    set :public, "public"
+
+    configure :production  do
+      ActiveRecord::Base.establish_connection(
+        :adapter    => 'sqlite3',
+        :database   => File.dirname( __FILE__) + '/db/unwatched.db'
+      )
+    end
+
+    configure :development do
+      ActiveRecord::Base.establish_connection(
+        :adapter    => 'sqlite3',
+        :database   => File.dirname( __FILE__) + '/db/unwatched.db'
+      )
+    end
+
+    configure :test do
+      ActiveRecord::Base.establish_connection(
+        :adapter    => 'sqlite3',
+        :database   => File.dirname( __FILE__) + '/db/unwatched_test.db'
+      )
+    end
+
+    #UnWatched::create_db
+
+
+    ####################################
+    ### ROUTES 
+    ####################################
+    ### Prefs
+    ####################################
+    get '/pref' do
+      @ext = UnWatched::Extension.all
+      erb :pref
+    end
+
+    post '/pref' do
+      @ext = UnWatched::Extension.parse_string( params['post']['ext'] )
+      erb :pref
+    end
+
+    get '/sort/:mode/?' do
+      session[:sort] = params[:mode].to_sym
+      redirect '/?path=' + get_path( params['path'] )
+    end
+
+    get '/mode/:mode/?' do
+      session[:mode] = params[:mode].to_sym
+      redirect '/?path=' + get_path( params['path'] )
+    end
+
+    ####################################
+    ### Main Route 
+    ####################################
+    get '/*' do
+      session_start!
+      session[:name] ||= 0
+      session[:name] += 1
+
+      session[:mode]   ||= :normal
+      session[:filter] ||= :normal
+      session[:sort]   ||= :alpha_asc 
+
+
+
+      @url                    = params['splat'].to_s
+
+      #Determine mode from url 
+      @mode, @merged, @sort   = discover_mode_from_url( @url )
+
+      @path      = get_path( params['path'] )
+
+      mark    =  params['mark']
+      mark    ||= false
+      puts mark
+      #Check if Media file, and launch if required.   
+      if not mark
+        media_test = open_media( @path )
+      else
+        #Mark files appropriatelly
+        media_test = mark_media( mark, @path, @mode)
       end
 
-      configure :development do
-         ActiveRecord::Base.establish_connection(
-            :adapter    => 'sqlite3',
-            :database   => File.dirname( __FILE__) + '/db/unwatched.db'
-         )
-      end
+      if media_test[:media]
+        ## Redirect, do not include the file to be launched.
+        ## This allows browser to be refreshed to get a new file list.
+        redirect "/" + params["splat"].to_s + "?path=" +  media_test[:path]
 
-      configure :test do
-         ActiveRecord::Base.establish_connection(
-            :adapter    => 'sqlite3',
-            :database   => File.dirname( __FILE__) + '/db/unwatched_test.db'
-         )
-      end
-     
-      ####################################
-      ### ROUTES 
-      ####################################
-      ### Prefs
-      ####################################
-      get '/pref' do
-         @ext = UnWatched::Extension.all
-         erb :pref
-      end
+      else
+        ## Create base_urls
+        @url_normal, @url_safe  = create_base_url( @url, @mode ) 
 
         ## Create urls for sorting links 
         @url_alpha, @url_mod    = create_sort_urls( @url.dup, session[:sort] ) 
 
+        ## create url for merged/unmerged views
+        @url_merged             = create_merged_url( @url, @merged )
 
-      
-      ####################################
-      ### Main Route 
-      ####################################
-      get '/*' do
-         @url                    = params['splat'].to_s
+        @files                  = get_files( @path.dup, @mode, session[:sort] )
 
-          #Determine mode from url 
-          @mode, @merged, @sort   = discover_mode_from_url( @url )
-         
-         @path      = get_path( params['path'] )
-          
-         mark    =  params['mark']
-         mark    ||= false
-         puts mark
-         #Check if Media file, and launch if required.   
-         if not mark
-            media_test = open_media( @path )
-         else
-            #Mark files appropriatelly
-            media_test = mark_media( mark, @path, @mode)
-         end
-            
-         if media_test[:media]
-            ## Redirect, do not include the file to be launched.
-            ## This allows browser to be refreshed to get a new file list.
-            redirect "/" + params["splat"].to_s + "?path=" +  media_test[:path]
-
-         else
-            #@url                    = params['splat'].to_s
-     
-            #Determine mode from url 
-            #@mode, @merged, @sort   = discover_mode_from_url( @url ) 
-                   
-            ## Create base_urls
-            @url_normal, @url_safe  = create_base_url( @url, @mode ) 
-
-            ## Create urls for sorting links 
-            @url_alpha, @url_mod    = create_sort_urls( @url.dup, @sort ) 
-
-            ## create url for merged/unmerged views
-            @url_merged             = create_merged_url( @url, @merged )
-
-            @files                  = get_files( @path.dup, @mode, @sort )
-
-            erb :all
-         end
+        erb :all
       end
+    end
 
-   end #class
+  end #class
 end #module
 
 
 
 if $0 == __FILE__
-   UnWatched::App.run!
+  UnWatched::App.run!
 end
 
--- unwatched.gemspec
+++ unwatched.gemspec
@@ -23,6 +23,7 @@ spec = Gem::Specification.new do |s|
    s.post_install_message = "To use '#{gem_name}' as a standalone application your gems folder must be on your path"   
    s.add_dependency("sinatra",">= 1.0.0")
    s.add_dependency("activerecord",">= 2.3.8")
+   s.add_dependency("sinatra-session",">= 1.0.0")
 
 end
 Rake::GemPackageTask.new(spec).define
